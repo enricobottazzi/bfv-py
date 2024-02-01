9bf0f15aa36570c0c1edf1bee19c11ed3fb2bdad
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2024-02-01 10:41:17 +0100

    chore: bump `0.1.9.3`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bfv",
-    version="0.1.9.2",
+    version="0.1.9.3",
     packages=find_packages(),
     install_requires=[
         "numpy",
